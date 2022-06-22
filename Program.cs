52ea34ae02915aaad638e475c5edc0685a030cc0
prescottlewis707 <prescottlewis707@gmail.com>
2022-06-22 09:25:32 +0100

    swagger

--- Program.cs
+++ Program.cs
@@ -16,11 +16,10 @@ var app = builder.Build();
 app.MapGet("/", () => "QA Rangers!");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
