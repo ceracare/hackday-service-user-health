5653cc7057df027b5d0ad22ae6db31807022c848
prescottlewis707 <prescottlewis707@gmail.com>
2022-06-22 12:17:18 +0100

    sleep

--- Controllers/SleepController.cs
+++ Controllers/SleepController.cs
@@ -29,17 +29,50 @@ public class SleepController : ControllerBase
         _logger = logger;
     }
 
+    [HttpPost(Name = "PostFitbitSleep")]
+    public async Task<String> Post()
+    {
+        using (var reader = new StreamReader(Request.Body))
+        {
+            var body = await reader.ReadToEndAsync();
+
+            Console.WriteLine(body);
+            // Do something
+            var sub = JsonNode.Parse(body);
+            var url = sub["SubscribeURL"];
+            return url.ToString();
+        }
+    }
+
     [HttpGet(Name = "GetSleep")]
-    public async Task Get()
+    public async Task<String> Get()
     {
 
-        Uri myUri = new Uri("https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=238LX9&redirect_uri=https%3A%2F%2Fapp-settings.fitbitdevelopercontent.com%2Fsimple-redirect.html&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight%20oxygen_saturation%20respiratory_rate&expires_in=6048000000");
-        // Create a 'HttpWebRequest' object for the specified url.
-        HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(myUri);
+        //Uri myUri = new Uri("https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=238LX9&redirect_uri=https%3A%2F%2Fapp-settings.fitbitdevelopercontent.com%2Fsimple-redirect.html&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight%20oxygen_saturation%20respiratory_rate&expires_in=6048000000");
+        //// Create a 'HttpWebRequest' object for the specified url.
+        //var authCode = "123";
+
+
+        //HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(myUri);
+        //webRequest.Method = "GET";
+        //String test = String.Empty;
+        //using (HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse())
+        //{
+        //    Stream dataStream = response.GetResponseStream();
+        //    StreamReader reader = new StreamReader(dataStream);
+        //    test = reader.ReadToEnd();
+        //    reader.Close();
+        //    dataStream.Close();
+
+        //    var redirect = response.ResponseUri.ToString();
+        //    Console.WriteLine(redirect);
+        //    authCode = redirect.Substring(redirect.IndexOf('='), redirect.IndexOf('#'));
+        //}
+        // HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(myUri);
         // Send the request and wait for response.
-        HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
-        var redirect = myHttpWebResponse.ResponseUri.ToString();
-        var authCode = redirect.Substring(redirect.IndexOf('='), redirect.IndexOf('#'));
+        //HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+        //var redirect = myHttpWebResponse.ResponseUri.ToString();
+        //var authCode = redirect.Substring(redirect.IndexOf('='), redirect.IndexOf('#'));
 
         int sleepDuration = 0;
 
@@ -48,40 +81,42 @@ public class SleepController : ControllerBase
             try
             {
 
-                var parameters = new Dictionary<string, string> { { "client_id", "238LX9" },
-                    { "grant_type", "authorization_code" },
-                    { "code", authCode } };
-                //Uzkoduojama URL'ui 
-                var encodedContent = new FormUrlEncodedContent(parameters);
-                //Post http callas.
-                HttpResponseMessage response = client.PostAsync("https://api.fitbit.com/oauth2/token", encodedContent).Result;
-                //nesekmes atveju error..
-                response.EnsureSuccessStatusCode();
-                //responsas to string
-                var responseToken = await response.Content.ReadFromJsonAsync<FitbitToken>();
+                //var parameters = new Dictionary<string, string> { { "client_id", "238LX9" },
+                //    { "grant_type", "authorization_code" },
+                //    { "code", authCode } };
+                ////Uzkoduojama URL'ui 
+                //var encodedContent = new FormUrlEncodedContent(parameters);
+                ////Post http callas.
+                //HttpResponseMessage response = client.PostAsync("https://api.fitbit.com/oauth2/token", encodedContent).Result;
+                ////nesekmes atveju error..
+                //response.EnsureSuccessStatusCode();
+                ////responsas to string
+                //var responseToken = await response.Content.ReadFromJsonAsync<FitbitToken>();
 
-                var token = responseToken.access_token;
+                //var token = responseToken.access_token;
 
                 var httpRequestMessage = new HttpRequestMessage
                 {
                     Method = HttpMethod.Get,
-                    RequestUri = new Uri("https://api.fitbit.com/1.2/user/-/sleep/date/2022-06-21.json"),
+                    RequestUri = new Uri("https://api.fitbit.com/1.2/user/-/sleep/date/2022-06-22.json"),
                     Headers = {
-            { HttpRequestHeader.Authorization.ToString(), $"Bearer {token}" }
+            { HttpRequestHeader.Authorization.ToString(), $"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhMWDkiLCJzdWIiOiIyWFE3U1ciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJyZXMgcm94eSByd2VpIHJociBybnV0IHJwcm8gcnNsZSIsImV4cCI6MTY1NTkyNDcwNywiaWF0IjoxNjU1ODk1OTA3fQ.6mHO2WVqAo8S6C_JgjAn40f1oDtypkgmSPxlZV9Hqs0" }
         }
                 };
 
                 var sleepResponse = client.SendAsync(httpRequestMessage).Result;
 
-                var sleepResult = response.Content.ReadAsStringAsync().Result;
+                var sleepResult = sleepResponse.Content.ReadAsStringAsync().Result;
                 var result = JsonNode.Parse(sleepResult);
+                Console.WriteLine(result);
                 sleepDuration = Int32.Parse(result["sleep"][0]["duration"].ToString());
-
+                return JsonNode.Parse($"{{ \"duration\": {sleepDuration} }}").ToJsonString();
             }
             catch (HttpRequestException e)
             {
                 Console.WriteLine("\nException Caught!");
                 Console.WriteLine("Message :{0} ", e.Message);
+                return JsonNode.Parse($"{{ \"duration\": {sleepDuration} }}").ToJsonString(); ;
             }
         }
 
